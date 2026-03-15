648acd394642ff98257ff858baffb05f2baa5fc0
Anil Kumar Ray <anilyahoo876@gmail.com>
2026-03-16 00:21:37 +0530

    responsive

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -3,7 +3,7 @@ import StopWatch from "./components/StopWatch";
 
 export default function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-linear-to-r from-purple-600 via-pink-400 to-red-500 flex items-cente justify-center">
       <StopWatch />
     </div>
   );
--- stopwatch/src/components/StopWatch.jsx
+++ stopwatch/src/components/StopWatch.jsx
@@ -9,8 +9,7 @@ export default function StopWatch() {
     setTime(0);
   };
 
-  // Let find minutes, second and milisecond
-  // Here 1 minutes == 60*1000 milisecond
+  // Calculate minutes, seconds, and milliseconds
   const minutes = Math.floor((time / 60000) % 60)
     .toString()
     .padStart(2, 0);
@@ -25,35 +24,34 @@ export default function StopWatch() {
     let interval;
     if (isRunning) {
       interval = setInterval(() => setTime((prevTime) => prevTime + 10), 10);
-      console.log(time);
     }
     return () => clearInterval(interval);
   }, [isRunning, time]);
 
   return (
-    <div className="flex min-h-full justify-center">
-      <div className="flex flex-col items-center p-6 rounded-xl bg-emerald-400 m-10 shadow-lg shadow-green-600 border-orange-600 border-4">
-        <h1 className="text-4xl font-bold shadow-lg p-4 mb-4 text-blue-800 rounded-xl border-l-orange-600 border-r-orange-600 border-l-8 border-r-8">
+    <div className="flex min-h-screen justify-center items-center p-4">
+      <div className="flex flex-col items-center p-6 rounded-xl bg-emerald-400 m-4 shadow-lg shadow-green-600 border-orange-600 border-4 w-full max-w-md sm:max-w-lg md:max-w-xl">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold shadow-lg p-4 mb-4 text-blue-800 rounded-xl border-l-orange-600 border-r-orange-600 border-l-8 border-r-8">
           STOP WATCH
         </h1>
-        <div className="text-5xl font-bold">
+        <div className="text-3xl sm:text-4xl md:text-5xl font-bold">
           {minutes} : {second} : {milisecond}
         </div>
-        <div className="w-fit flex m-8 gap-8">
+        <div className="w-full flex flex-wrap justify-center gap-4 mt-8">
           <button
-            className="shadow-lg shadow-lime-800 p-[7px_20px] rounded-xl font-bold hover:bg-green-400 "
+            className="shadow-lg shadow-lime-800 p-2 sm:p-[7px_20px] rounded-xl font-bold hover:bg-green-400"
             onClick={() => setIsRunning(true)}
           >
             Start
           </button>
           <button
-            className="shadow-lg shadow-lime-800 p-[7px_20px] rounded-xl font-bold hover:bg-green-400 "
+            className="shadow-lg shadow-lime-800 p-2 sm:p-[7px_20px] rounded-xl font-bold hover:bg-green-400"
             onClick={() => setIsRunning(false)}
           >
             Stop
           </button>
           <button
-            className="shadow-lg shadow-lime-800 p-[7px_20px] rounded-xl font-bold hover:bg-green-400 "
+            className="shadow-lg shadow-lime-800 p-2 sm:p-[7px_20px] rounded-xl font-bold hover:bg-green-400"
             onClick={handlerReset}
           >
             Reset
